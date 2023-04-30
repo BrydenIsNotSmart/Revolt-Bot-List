3fd837da6931310dda28e894bacfebc1eee5ebf5
BrydenIsNotSmart <bryden@vitaldevs.org>
2023-04-30 00:33:55 -0500

    Recent Changes

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -37,12 +37,17 @@ module.exports = {
         let voteReset = new CronJob(
         '0 0 1 * *',
          async function() {
-            let bots = await botModel.find();
-			let month = { 0: "December", 1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August" };
-			await client.api.post(`/channels/01GQ15314DTQ68KXXG942648WG/messages`, {
-			content:
-				`# Vote Reset\nThe monthly vote count has been reset!\n\n Congratulations to the following bots for being the **Most Voted Bots of __${month[new Date().getMonth()]}__**`
-		   }).catch(() => { });
+			let bots = await botModel.find();
+            let month = { 0: "December", 1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November" };
+            bots.sort((a, b) => b.monthlyVotes - a.monthlyVotes);
+            let top5 = bots.slice(0, 5); // get the top 5 bots
+            let content = `# Vote Reset\nThe monthly vote count has been reset!\n Congratulations to the following bots for being the **Most Voted Bots of __${month[new Date().getMonth()]}__**.\n\n\n## ${month[new Date().getMonth()]} Leaderboard`;
+            top5.forEach((bot, index) => {
+            content += `${index + 1}. ${bot.name} - ${bot.monthlyVotes} monthly votes, ${bot.votes} votes in total\n`;
+            });
+            await client.api.post(`/channels/01GQ15314DTQ68KXXG942648WG/messages`, {
+            content
+            }).catch(() => { });
 		     	  await bots.forEach(async (a) => {
 			      await botModel.findOneAndUpdate(
 				    {
