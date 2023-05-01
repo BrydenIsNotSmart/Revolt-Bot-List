250e3d1f5c1dba19453f044fa5ed27ae058e238f
BrydenIsNotSmart <bryden@vitaldevs.org>
2023-05-01 16:21:07 -0500

    changes

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -38,10 +38,10 @@ module.exports = {
         '0 0 1 * *',
          async function() {
 			let bots = await botModel.find();
-            let month = { 0: "December", 1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November" };
+            let month = { 0: "January", 1: "February", 2: "March", 3: "April", 4: "May", 5: "June", 6: "July", 7: "August", 8: "September", 9: "October", 10: "November", 11: "December" };
             bots.sort((a, b) => b.monthlyVotes - a.monthlyVotes);
             let top5 = bots.slice(0, 5); // get the top 5 bots
-            let content = `# Vote Reset\nThe monthly vote count has been reset!\n Congratulations to the following bots for being the **Most Voted Bots of __${month[new Date().getMonth()]}__**.\n\n\n## ${month[new Date().getMonth()]} Leaderboard`;
+            let content = `# Vote Reset\nThe monthly vote count has been reset!\n Congratulations to the following bots for being the **Most Voted Bots of __${month[new Date().getMonth()]}__**.\n\n\n## ${month[new Date().getMonth()]} Leaderboard\n`;
             top5.forEach((bot, index) => {
             content += `${index + 1}. ${bot.name} - ${bot.monthlyVotes} monthly votes, ${bot.votes} votes in total\n`;
             });
