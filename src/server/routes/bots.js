b89d13027d2ca2b021f07c757a7c6add78e17239
NightRaven <53287573+Xynnix@users.noreply.github.com>
2023-05-03 20:13:26 -0400

    Update bots.js

--- src/server/routes/bots.js
+++ src/server/routes/bots.js
@@ -92,10 +92,10 @@ router.post('/submit', checkAuth, async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     let approved = await botModel.findOne({ id: req.params.id, status: "approved" }) || await botModel.findOne({ name: req.params.id, status: "approved", certified: true });
-    let pending = await botModel.findOne({ id: req.params.id, status: "awaiting" });
+    let awaiting = await botModel.findOne({ id: req.params.id, status: "awaiting" });
     let isStaff = await userModel.findOne({ revoltId: req.session.userAccountId, isStaff: true });
-    if (!approved && !pending || pending && !isStaff && !pending.owners.includes(req.session.userAccountId)) return res.status(404).json({ message: "This bot could not be found on our list or is not approved."})
-    let bot = pending || approved;
+    if (!approved && !awaiting || awaiting && !isStaff && !awaiting.owners.includes(req.session.userAccountId)) return res.status(404).json({ message: "This bot could not be found on our list or is not approved."})
+    let bot = awaiting || approved;
     const marked = require("marked");
     const description = marked.parse(bot.description);
     bot.description = description;
