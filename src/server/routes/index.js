558293704c48bca54e394528d407822fd810c4b9
sldless <67599596+sldless@users.noreply.github.com>
2023-05-05 22:38:55 -0400

    added support for usernames

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -47,16 +47,20 @@ router.get('/explore', async (req, res) => {
   })
 })
 
-router.get("beta/search", async (req, res) => {
+router.get("/search", async (req, res) => {
   const search = req.query?.q || req.query?.s;
   if (!search) return res.redirect("/");
 
   let bots = await model.find({ approved: true });
   let botsList = [];
-  let user = await userModel.findOne({ revoltId: req.session.userAccountId }) || null;
+  let user = (await userModel.findOne({ revoltId: req.session.userAccountId })) || null;
   for (let i = 0; i < bots.length; i++) {
+    const BotRaw = (await client.bots.fetchPublic(bots[i].id).catch(() => {})) || null;
+    bots[i].name = BotRaw.username;
+    bots[i].avatar = BotRaw.avatar;
     if (
       bots[i].tags.map((t) => t.toLowerCase()).includes(search.toLowerCase()) ||
+      BotRaw.username.toLowerCase().includes(search.toLowerCase()) ||
       bots[i].shortDesc.toLowerCase().includes(search.toLowerCase()) ||
       bots[i].id.toLowerCase().includes(search.toLowerCase())
     ) {
@@ -64,6 +68,7 @@ router.get("beta/search", async (req, res) => {
     }
     bots[i].tags = bots[i].tags.join(", ");
   }
+console.log(search, botsList)
   res.render("search.ejs", {
     bot: global.client,
     bots: botsList,
